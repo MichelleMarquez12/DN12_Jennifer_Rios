e46e7cd38896fa39ad3538f218e609e0ea5479bb
MichelleMarquez12 <marquezmichel282@gmail.com>
2024-01-22 18:09:07 -0600

    Tutorial uno
    Tutorial uno, creación de proyectos c#, librería, clases,  instanciación, uso de métodos y llamadas dentro de otro proyecto.

---
+++ M1_ObjectOrientedProgramming/T1/OOP.ConsoleExample/OOP.ConsoleExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Permite hacer uso del contenido de AudioManager
+//Allows you to use AudioManager content
+using AudioManager;
+
+//first c# project usage tutorial
+namespace OOP.ConsoleExample
+{
+    //The internal class modifier defines that this class is available to elements of the same project.
+    //el modificador de clase internal, define que esa clase esta disponible a elementos del mismo proyecto
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //console.writeLine class for printing text 
+            Console.WriteLine("Hello Wordl!");
+
+            //Instancia de la clase Animal
+            //Instance of Animal class
+            Animal animal = new Animal();
+
+            //Llamada al metodo de la clase animal
+            //Call to the animal class method
+            animal.AnimalSound();
+
+            //ReadKey allows the program to close once the user presses a key
+            Console.ReadKey();
+        }
+    }
+}
