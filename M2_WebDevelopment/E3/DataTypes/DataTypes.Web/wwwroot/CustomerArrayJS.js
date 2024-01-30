7ce0f9d668c746a785e373a5d309024db9e28a68
MichelleMarquez12 <20690029@tecvalles.mx>
2024-01-30 12:10:54 -0600

    EJERCICIO 3
    Creación de clase Costumer para impresión de lista de objetos en distintos ciclos, así como la creación, de la vista, controlador de Costumer para muestra de lista de los objetos de prueba.

---
+++ M2_WebDevelopment/E3/DataTypes/DataTypes.Web/wwwroot/CustomerArrayJS.js
@@ -0,0 +1,55 @@
+﻿(function() {
+    // Crear un array con las mismas propiedades de la clase Customer de C#
+    var customers = [
+        { Name: "Michelle", RegistrationDate: "2024/01/29"},
+        { Name: "David", RegistrationDate: "2024/02/14"},
+        { Name: "Juan", RegistrationDate: "2023/05/19"},
+        { Name: "Dulce", RegistrationDate: "2021/01/30"},
+        { Name: "Melania", RegistrationDate: "2020/09/19"},
+        { Name: "Daniel", RegistrationDate: "2019/11/20"},
+        { Name: "Miguel", RegistrationDate: "2018/12/01"},
+        { Name: "Mario", RegistrationDate: "2017/06/03"},
+        { Name: "Esther", RegistrationDate: "2016/10/15"},
+        { Name: "Daniela", RegistrationDate: "2015/07/12"}
+    ];
+
+    // Recorrer el array con el ciclo 'for' y agregar un tag <li> dentro de un tag <ul> en la pantalla por cada elemento
+    var customerList = document.getElementById("customerList");
+    for (var i = 0; i < customers.length; i++) {
+        var listItem = document.createElement("li");
+        listItem.textContent = "Nombre: " + customers[i].Name + ", Fecha de registro: " + customers[i].RegistrationDate;
+        customerList.appendChild(listItem);
+    }
+} ());
+
+(function () {
+
+    var bv = new Bideo();
+    bv.init({
+        // Video element
+        videoEl: document.querySelector('#background_video'),
+
+        // Container element
+        container: document.querySelector('body'),
+
+        // Resize
+        resize: true,
+
+        // autoplay: false,
+        
+
+        // Array of objects containing the src and type
+        // of different video formats to add
+        src: [
+            {
+                src: '/lib/bideo/night.mp4',
+                type: 'video/mp4'
+            }
+        ],
+
+        // What to do once video loads (initial frame)
+        onLoad: function () {
+            document.querySelector('#video_cover').style.display = 'none';
+        }
+    });
+}());
---
+++ M2_WebDevelopment/E3/DataTypes/DataTypes/Customer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DataTypes
+{
+    //Crear una colección genérica de la clase Customer. 
+    public class Customer
+    {
+        public string Name { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Customer> customers = new List<Customer>();
+
+            //Llenarla con 10 datos de prueba
+            customers.Add(new Customer { Name = "Michelle", RegistrationDate = DateTime.Now });
+            customers.Add(new Customer { Name = "David", RegistrationDate = DateTime.Now });
+            customers.Add(new Customer { Name = "Juan", RegistrationDate = DateTime.Now });
+            customers.Add(new Customer { Name = "Dulce", RegistrationDate = DateTime.Now });
+            customers.Add(new Customer { Name = "Melania", RegistrationDate = DateTime.Now });
+            customers.Add(new Customer { Name = "Daniel", RegistrationDate = DateTime.Now });
+            customers.Add(new Customer { Name = "Miguel", RegistrationDate = DateTime.Now });
+            customers.Add(new Customer { Name = "Mario", RegistrationDate = DateTime.Now });
+            customers.Add(new Customer { Name = "Esther", RegistrationDate = DateTime.Now });
+            customers.Add(new Customer { Name = "Daniela", RegistrationDate = DateTime.Now });
+
+            //Recorrerla e imprimir en pantalla el nombre del cliente y su fecha de registro con el bucle foreach.
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Bucle FOREACH\n");
+                Console.WriteLine("Nombre: " + customer.Name + ", Fecha de registro: " + customer.RegistrationDate);
+                Console.WriteLine("\n");
+            }
+
+            //Recorrerla e imprimir en pantalla el nombre del cliente y su fecha de registro con el bucle for. 
+            for (int i = 0; i < customers.Count; i++)
+            {
+                Console.WriteLine("Bucle FOR \n");
+                Console.WriteLine("Nombre: " + customers[i].Name + ", Fecha de registro: " + customers[i].RegistrationDate);
+                Console.WriteLine("\n");
+            }
+
+            //Recorrerla e imprimir en pantalla el nombre del cliente y su fecha de registro con el bucle while.
+            int c = 0;
+            while (c < customers.Count)
+            {
+                Console.WriteLine("Bucle WHILE");
+                Console.WriteLine("Nombre: " + customers[c].Name + ", Fecha de registro: " + customers[c].RegistrationDate);
+                c++;
+                Console.WriteLine("\n");
+            }
+
+            //Recorrerla e imprimir en pantalla el nombre del cliente y su fecha de registro con el bucle do while.
+            int x = 0;
+            do
+            {
+                Console.WriteLine("Bucle DO WHILE\n");
+                Console.WriteLine("Nombre: " + customers[x].Name + ", Fecha de registro: " + customers[x].RegistrationDate);
+                x++;
+            } while (x < customers.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
