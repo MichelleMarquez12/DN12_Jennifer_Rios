f5f1d8493634c7506915ba371ca96f5403cc06eb
MichelleMarquez12 <20690029@tecvalles.mx>
2024-02-02 16:19:28 -0600

    TUTORIAL 1
    Diseños (Layouts)

---
+++ M3_NetProjectsWebApi/T1/GymManager.Web/GymManager.Web/Models/City.cs
@@ -0,0 +1,9 @@
+﻿namespace GymManager.Web.Models
+{
+    public class City
+    {
+        //Propiedades de las ciudades
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ M3_NetProjectsWebApi/T1/GymManager.Web/GymManager.Web/Program.cs
@@ -0,0 +1,42 @@
+
+
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Agregar los servicios requeridos
+builder.Services.AddRazorPages();
+
+// Resto de la configuración del servidor y la aplicación
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Map("/Members", app =>
+{
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Members}/{action=Index}/{id?}"
+        );
+    });
+});
+
+
+app.MapRazorPages();
+app.UseStaticFiles();
+
+app.Run();
