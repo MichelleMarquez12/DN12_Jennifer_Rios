cac4ac06f1f07c406c282e772b67b2f69758c6c3
MichelleMarquez12 <20690029@tecvalles.mx>
2024-02-07 01:27:22 -0600

    TUTORIAL 7
    Autenticación, validación y creación de usuario para logeo.

---
+++ M3_NetProjectsWebApi/T7/GymManager.Web/GymManager.Core/Members/Member.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.Core.Members
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        [StringLength (25)]
+
+        [Required(ErrorMessage = "Enter member name, try again")]
+        public string Name { get; set; }
+
+        [StringLength(25)]
+        [Required(ErrorMessage = "Enter member Lastname, try again")]
+        public string LastName { get; set; }
+
+        [BindProperty, DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDay { get; set; }
+
+        //Es de tipo int porque en vista se presentara como liista despegable de ciudades
+        //por ello se crea su modelo City.cs
+        [Range (1, 100)]
+        public int CityId { get; set; }
+
+        [EmailAddress]
+
+        [Required(ErrorMessage = "Enter member Email, try again")]
+        public string Email { get; set; }
+        public bool AllowsNewsLetter { get; set; }
+    }
+}
