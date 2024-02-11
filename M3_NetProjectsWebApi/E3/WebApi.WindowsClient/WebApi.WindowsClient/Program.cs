911e64e2b6ca9f641f6c0fcd8f079f9c7fef2ce7
MichelleMarquez12 <20690029@tecvalles.mx>
2024-02-10 20:07:09 -0600

    EJERCICIO 3
    Peticiones hacia API's haciendo uno de Postman, API del clima con prueba de API OpenWeatherMap, con peticiones mediante nombre del lugar, ID así como longitud y latitud del lugar. API Weatherforecast con petición de datos mediante postman. API de traducción de textos (Traducir plus)

---
+++ M3_NetProjectsWebApi/E3/WebApi.WindowsClient/WebApi.WindowsClient/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+namespace WebApi.WindowsClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GetIp();
+
+            Console.ReadKey();
+
+        }
+
+
+        //Muestra solo la ip del dispositivo
+        public static async Task<IpAddress> GetIp()
+        {
+            //INSTANCIA A LA CLASE HTTP
+            HttpClient client = new HttpClient();
+
+            //Invocación al método
+            HttpResponseMessage response = await client.GetAsync("https://api.ipify.org/?format=json");
+
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            IpAddress ip = Newtonsoft.Json.JsonConvert.DeserializeObject<IpAddress>(responseBody);
+
+            Console.WriteLine("My current ip address is: " + ip.Ip);
+
+            var info = await GetIpInfo(ip.Ip);
+
+            Console.WriteLine("City: " + info.City);
+            Console.WriteLine("Country: " + info.Country);
+            Console.WriteLine("Timezone: " + info.Timezone);
+
+            return ip;
+        }
+
+        //Muestra ip y informacion del dispositivo
+        public static async Task<IpInfo> GetIpInfo(string IpAddress)
+        {
+            //INSTANCIA A LA CLASE HTTP
+            HttpClient client = new HttpClient();
+
+            //Invocación al método
+            HttpResponseMessage response = await client.GetAsync($"https://ipinfo.io/{IpAddress}/geo");
+
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            IpInfo info = Newtonsoft.Json.JsonConvert.DeserializeObject<IpInfo>(responseBody);
+
+            return info;
+        }
+
+    }
+}
