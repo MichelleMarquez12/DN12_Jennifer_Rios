a6d32dd838995695e0651ad80ea4ccfc0692b220
MichelleMarquez12 <20690029@tecvalles.mx>
2024-04-04 17:19:12 -0600

    TUTORIAL 9
    Creación de reporte en formato pdf con .net descargable de los nuevos miembros del Gym en determinado periodo.

---
+++ M6_NetCoreWithEntityFramework/T9/GymManager.Web/GymManager.Web/Models/MemberViewModel.cs
@@ -0,0 +1,38 @@
+﻿using GymManager.Core.Members;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace GymManager.Web.Models
+{
+    public class MemberViewModel
+    {
+        public int Id { get; set; }
+
+        [StringLength(45)]
+
+        [Required(ErrorMessage = "Enter member name, try again")]
+        public string Name { get; set; }
+
+        [StringLength(45)]
+        [Required(ErrorMessage = "Enter member Lastname, try again")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+
+        [Required(ErrorMessage = "Enter member Email, try again")]
+        public string Email { get; set; }
+
+        [Phone]
+
+        [Required(ErrorMessage = "Enter phone number, try again")]
+        public string Phone { get; set; }
+
+        [BindProperty, DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaRegistro { get; set; }
+
+        [Range(1, 100)]
+        public int IdCities { get; set; }
+
+        public bool AllowsNewsLetter { get; set; }
+    }
+}
---
+++ M6_NetCoreWithEntityFramework/T9/GymManager.Web/GymManager.Web/wwwroot/JS.js
@@ -0,0 +1,40 @@
+﻿document.getElementById('modalButton').addEventListener('click', function () {
+    var identificador = info.value;
+    // Obtener la hora actual
+    var fecha = new Date();
+    var hora = fecha.getHours();
+    var minutos = fecha.getMinutes();
+    var segundos = fecha.getSeconds();
+    // Crear el mensaje de la alerta
+    var mensaje = "Hola: " + " " + identificador + " , a las: " + hora + ":" + minutos + ":" + segundos;
+    $("#mensajeModal").text(mensaje);
+    $('#myModal').modal('show');
+
+    setTimeout(function () {
+        $('#myModal').modal('hide');
+    }, 3000);
+});
+
+
+$("#info").on('keydown', function (e) {
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == 13) {
+        var identificador = info.value;
+        var fecha = new Date();
+        var hora = fecha.getHours();
+        var minutos = fecha.getMinutes();
+        var segundos = fecha.getSeconds();
+        var mensaje1 = "Hola: " + " " + identificador + " , a las: " + hora + ":" + minutos + ":" + segundos;
+
+        // Mostrar el mensaje en el modal
+        $("#mensajeModal").text(mensaje1);
+
+        // Mostrar el modal
+        $('.modal').modal('show');
+
+        // Cerrar el modal después de 3 segundos
+        setTimeout(function () {
+            $('.modal').modal('hide');
+        }, 3000);
+    }
+});
