d130e2c898a5f386a08fe8f618af5870da3e0043
MichelleMarquez12 <20690029@tecvalles.mx>
2024-04-04 17:47:39 -0600

    TUTORIAL 10
    Adición de grafica que muestra los datos del reporte de miembros, por membresia en determinado periodo.

---
+++ M6_NetCoreWithEntityFramework/T10/GymManager.Web/GymManager.Web/Controllers/MembersController.cs
@@ -0,0 +1,71 @@
+﻿using GymManager.ApplicationServices.Members;
+using GymManager.Core.Members;
+using GymManager.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace GymManager.Web.Controllers
+{
+    [Authorize]
+    public class MembersController : Controller
+    {
+        private readonly IMembersAppService _membersAppService;
+
+        public MembersController(IMembersAppService membersAppService)
+        {
+            _membersAppService = membersAppService;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            List<Member> members  = await _membersAppService.GetMembersAsync();
+
+            MemberListViewModel viewModel = new MemberListViewModel();
+
+            viewModel.NewMembersCount = 2;
+            viewModel.Members = members;
+
+            Log.Information("User executed Member's Index");
+
+            return View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(int memberId)
+        {
+            Member member = await _membersAppService.GetMemberAsync(memberId);
+
+            MemberListViewModel viewModel = new MemberListViewModel();
+
+            return View(member);
+        }
+
+
+        public async Task<IActionResult> Delete(int memberId)
+        {
+            await _membersAppService.DeleteMemberAsync(memberId);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Member member)
+		{
+            await _membersAppService.AddMemberAsync(member);
+			return RedirectToAction("Index");
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Member member)
+        {
+            await _membersAppService.EditMemberAsync(member);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ M6_NetCoreWithEntityFramework/T10/GymManager.Web/GymManager.Web/Models/MembershipTypesListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GymManager.Core.Members;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GymManager.Web.Models
+{
+    public class MembershipTypesListViewModel
+    {
+        public int NewMembershipTypesCount { get; set; }
+
+        public List<MembershipTypes> MembershipTypess { get; set; }
+    }
+}
